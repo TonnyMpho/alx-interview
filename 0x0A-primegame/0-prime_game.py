0d57dcad8b0c5f486d93c113bd9bf4df921ae23d
TonnyMpho <tonnympho1@gmail.com>
2024-03-12 19:02:35 +0000

    Prime Game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -13,46 +13,40 @@ def is_prime(num):
             return False
     return True
 
-
 def isWinner(x, nums):
-    """ Prime Game - winner function to determine the winner """
-
-    def optimal_move(remaining):
-        """
-        Determines the optimal move for the current player.
-        """
-        for num in remaining:
-            if is_prime(num):
-                return num
-        return None
-
-    def play_game(n):
-        """
-        Simulates a single round of the game.
-        """
-        maria_turn = True
-        rem_nums = list(range(1, n + 1))
-        while True:
-            prime = optimal_move(rem_nums)
-            if prime is None:
-                return not maria_turn
-            rem_nums = [num for num in rem_nums if num % prime != 0]
-            maria_turn = not maria_turn
-            if not rem_nums:
-                return maria_turn
-
+    """
+    Determines the winner of each game based on the rules described.
+    """
     maria_wins = 0
     ben_wins = 0
 
     for n in nums:
-        if play_game(n):
-            maria_wins += 1
-        else:
-            ben_wins += 1
+        maria_turn = True
+        while n > 1:
+            prime = 2
+            while prime <= n:
+                if is_prime(prime):
+                    break
+                prime += 1
+
+            if prime > n:
+                if maria_turn:
+                    ben_wins += 1
+                else:
+                    maria_wins += 1
+                break
+            n -= prime * (n // prime)
+            maria_turn = not maria_turn
+        
+        if n == 1:
+            if maria_turn:
+                ben_wins += 1
+            else:
+                maria_wins += 1
 
     if maria_wins > ben_wins:
-        return "Maria"
+        return 'Maria'
     elif ben_wins > maria_wins:
-        return "Ben"
+        return 'Ben'
     else:
         return None
